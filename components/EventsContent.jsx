b6cd7cf532e47aca3088fdaeb6a3b19d4d703318
m m <m m>
2016-11-15 11:16:33 -0600

    output team events in human readable format

--- components/EventsContent.jsx
+++ components/EventsContent.jsx
@@ -15,21 +15,38 @@ export default class EventsContent extends React.Component {
 
   render() {
     let contentNodes = []
-    this.props.content.forEach((item, index) => {
+    let eventNodes =  this.filterEventsByTeam(this.props.selectedTeam)
+    //Translates league keys to human readable format ONLY FOR ENGLISH LEAGUES SO FAR - wrap in switch statement for res[0] === "en", "at", "es"...
+    eventNodes.forEach((item) => {
+      let res = item.split(".")
+      if (res.length === 2) {
+        res[2] = res [1]
+        res[1] = "Premier League"
+      }
+      else if (res[1] === "2") {
+        res[1] = "Championship"
+      }
+      else if (res[1] === "3") {
+        res[1] = "League 1"
+      }
+      else if (res[1] === "4") {
+        res[1] = "League 2"
+      }
+
       let node = (
-        <div key={item.key}>
-        	<ul>
-        	<li>key={item.key}</li>
-        	<li>title={item.title}</li>
-        	</ul>
-        </div>
+        <li>{res[1]} {res[2]}</li>
       )
       contentNodes.push(node)
     })
     return (
       <div>
         <p>selected team = {this.props.selectedTeam}</p>
-        <p>events = {this.filterEventsByTeam(this.props.selectedTeam)}</p>
+        <div>
+          events = 
+          <ul>
+            {contentNodes}
+          </ul>
+        </div>
       </div>
     )
   }
